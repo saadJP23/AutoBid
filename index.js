8b2914aa7002da056d47dd1df2f6f75f4dc71bb1
saadJP23 <149016761+saadJP23@users.noreply.github.com>
2023-11-18 21:16:11 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+$("a").on("mouseenter", function() {
+    $(this).css("color", "white");
+}).on("mouseleave", function() {
+    $(this).css("color", "black");
+})
+
+$(".contact").on("mouseenter", function() {
+    $(this).css("color", "black");
+    }).on("mouseleave", function() {
+        $(this).css("color", "white");
+    })
+
+$(".contact").on("mouseenter", function() {
+    $(this).css("background-color", "white");
+    }).on("mouseleave", function() {
+        $(this).css("background-color", "black");
+    })
+
+
+$(".dropdown-item").on("mouseenter", function() {
+    $(this).css("background-color", "black");
+    }).on("mouseleave", function() {
+        $(this).css("background-color", "white");
+    })
+
+$(".dropdown-item").on("mouseenter", function() {
+    $(this).css("background-color", "black");
+    }).on("mouseleave", function() {
+        $(this).css("background-color", "white");
+    })
+    
